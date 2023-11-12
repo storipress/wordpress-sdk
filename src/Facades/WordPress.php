ed7c7915cdd0174a9819247b244377ff9dbbbc16
Lyrisbee <Lyrisbee@users.noreply.github.com>
2023-11-13 00:01:39 +0800

    update facades

--- src/Facades/WordPress.php
+++ src/Facades/WordPress.php
@@ -5,7 +5,25 @@ declare(strict_types=1);
 namespace Storipress\WordPress\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Storipress\WordPress\Requests\Category;
+use Storipress\WordPress\Requests\Post;
+use Storipress\WordPress\Requests\Tag;
+use Storipress\WordPress\Requests\User;
 
+
+/**
+ * @method static Post post()
+ * @method static Category category()
+ * @method static Tag tag()
+ * @method static User user()
+ * @method static \Storipress\WordPress\WordPress setSite(string $site)
+ * @method static \Storipress\WordPress\WordPress site()
+ * @method static \Storipress\WordPress\WordPress setUsername(string $username)
+ * @method static \Storipress\WordPress\WordPress username()
+ * @method static \Storipress\WordPress\WordPress setApplicationKey(string $key)
+ * @method static \Storipress\WordPress\WordPress applicationKey()
+ * @method static \Storipress\WordPress\WordPress instance()
+ */
 class WordPress extends Facade
 {
     /**
